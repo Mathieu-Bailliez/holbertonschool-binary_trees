dc72fdbd43ec2b3ef4d2b17c968c96e38b691e4c
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-11 15:40:39 +0200

    16. fix: add helper functions inside file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,58 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node
+ *
+ * Return: Height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ *
+ * @tree: Pointer to the root node
+ *
+ * Return: Size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) +
+		binary_tree_size(tree->right));
+}
+
+/**
+ * _pow_perfect - Returns 2 to the power of n
+ *
+ * @n: The exponent
+ *
+ * Return: 2^n
+ */
+static size_t _pow_perfect(size_t n)
+{
+	if (n == 0)
+		return (1);
+
+	return (2 * _pow_perfect(n - 1));
+}
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  *
  * @tree: Pointer to the root node of the tree to check
@@ -12,18 +64,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	size_t height;
 	size_t size;
 	size_t perfect_size;
-	size_t i;
 
 	if (!tree)
 		return (0);
 
 	height = binary_tree_height(tree);
 	size = binary_tree_size(tree);
-
-	perfect_size = 1;
-	for (i = 0; i <= height; i++)
-		perfect_size *= 2;
-	perfect_size -= 1;
+	perfect_size = _pow_perfect(height + 1) - 1;
 
 	return (size == perfect_size);
 }
